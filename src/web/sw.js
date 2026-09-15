b0d81cd13149f8c0eff21ee11a144e488f41b23b
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-15 03:23:23 -0400

    Cache only the static PWA shell

---
+++ src/web/sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'passman-shell-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './app.js',
+  './ui.css',
+  './app.css',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.origin !== location.origin) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(fetch(request).catch(() => caches.match('./index.html')));
+    return;
+  }
+
+  event.respondWith(caches.match(request).then(cached => cached || fetch(request)));
+});
