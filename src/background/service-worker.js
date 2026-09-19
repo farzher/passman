38ccb7dd1c9d56662e5eb04b1c0e0a4cb4603f25
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-19 19:35:12 -0400

    Sync master password rewraps safely

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -296,6 +296,7 @@ async function handle(message, sender) {
     try {
       ({ key: verifiedKey } = await unlockEnvelope(message.currentPassword, envelope));
       await setEnvelope(await rewrapKey(message.password, verifiedKey, envelope));
+      await setSettings({ syncDirty: true });
     } finally {
       verifiedKey?.fill(0);
     }
